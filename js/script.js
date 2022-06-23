1d58805b6e5ea7478e4256f0b05e8027d279d52f
WallSid <103652593+WallSid@users.noreply.github.com>
2022-06-23 12:41:35 +0300

    миграция спойлера и слайдера в мейн ветку + работа над геллереей

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Получаем объект body
 
 const bodyElement = document.body;
@@ -75,4 +76,235 @@ console.log(elemsThree);
 
 // 1st nesting level selector
 const elemsFour = document.querySelectorAll('.Services__list-block>div');
-// console.log(elemsFour);
+// console.log(elemsFour);// console.log(elemsFour);
+=======
+'use strict'
+    new Swiper('.text-slider', {
+    slidesPerView: 1,
+    spaceBetween: 150,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+    // Responsive breakpoints
+  });
+
+
+// spollers
+const spollersArray  = document.querySelectorAll('[data-spollers]');
+if(spollersArray.length > 0) {
+    // getting regular spollers
+    const spollersRegular = Array.from(spollersArray).filter(function (item, index, self) {
+        return !item.dataset.spollers.split(",")[0];
+    });
+    // init regular spollers
+    if (spollersRegular.length > 0) {
+        initSpollers(spollersRegular);
+    }
+
+    // getting media spoller
+    const spollersMedia = Array.from(spollersArray).filter(function(item, index, self) {
+        return item.dataset.spollers.split(",")[0];
+    });
+        // init spollers with media request
+    if (spollersMedia.length > 0) {
+        const breakpointsArray = [];
+        spollersMedia.forEach(item => {
+            const params = item.dataset.spollers;
+            const breakpoint = {};
+            const paramsArray = params.split(",");
+            breakpoint.value = paramsArray[0];
+            breakpoint.type = paramsArray[1] ? paramsArray[1].trim() : "max";
+            breakpoint.item = item;
+            breakpointsArray.push(breakpoint);
+        });
+
+        // getting unique breakepoints
+        let mediaQueries = breakepointsArray.map(function (item) {
+            return '(' + item.type + '-width: ' + item.value + 'px),' + ',' + item.type;
+        });
+        mediaQueries = mediaQueries.filter(function (item, index, self) {
+            return self.indexOf(item) === index;
+        });
+
+        // works with every breakepoints...
+        mediaQueries.forEach(breakpoint => {
+            const paramsArray = breakpoint.split(",");
+            const mediaBreakpoint = paramsArray[1];
+            const mediaType = paramsArray[2];
+            const matchMedia = window.matchMedia(paramsArray[0]); 
+
+            //object with required condition
+            const spollersArray = breakpointsArray.filter(function (item) {
+                if (item.value === mediaBreakpoint && item.type === mediaType) {
+                    return true;
+                }
+            });
+            // Event(событие)
+            matchMedia.addListener(function () {
+                initSpollers(spollersArray, matchMedia);
+            });
+            initSpollers(spollersArray, matchMedia);
+        });
+    }
+
+    // initializing
+    function initSpollers(spollersArray, matchMedia = false) {
+        spollersArray.forEach(spollersBlock => {
+            spollersBlock = matchMedia ? spollersBlock.item : spollersBlock;
+            if (matchMedia.matches || !matchMedia) {
+                spollersBlock.classList.add("_init");
+                initSpollerBody(spollersBlock);
+                spollersBlock.addEventListener("click", setSpollerAction);
+            }   else {
+                spollersBlock.classList.remove('_init');
+                initSpollerBody(spollersBlock, false);
+                spollersBlock.removeEventListener("click", setSpollerAction);
+                console.log(spollersBlock)
+            }
+        });
+    }
+
+    //works with content
+    function initSpollerBody(spollerBlock, hideSpollerBody = true) {
+        const spollerTitles = spollerBlock.querySelectorAll("[data-spoller]");
+        if (spollerTitles.length > 0) {
+            spollerTitles.forEach(spollerTitle => {
+                if (hideSpollerBody) {
+                    spollerTitle.removeAttribute("tabindex");
+                    if (!spollerTitle.classList.contains("_active")) {
+                        spollerTitle.nextElementSibling.hidden = true;
+                    }
+                }   else {
+                    spollerTitle.setAttribute("tabindex", "-1");
+                    spollerTitle.nextElementSibling.hidden = false;
+                }
+            });
+        }
+    }
+
+    function setSpollerAction(e) {
+        const el1 = e.target;
+        if (el1.hasAttribute("data-spoller") || el1.closest("[data-spoller]")) {
+            const spollerTitle = el1.hasAttribute("data-spoller") ? el1 : el1.closest("[data-spoller]");
+            const spollersBlock = spollerTitle.closest("[data-spollers");
+            const oneSpoller = spollersBlock.hasAttribute("data-one-spoller") ? true : false;
+            if (!spollersBlock.querySelectorAll("._slide").length) {
+                if (oneSpoller && !spollerTitle.classList.contains("_active")) {
+                    hideSpollersBody(spollersBlock);
+                }
+                spollerTitle.classList.toggle("_active");
+                _slideToggle(spollerTitle.nextElementSibling, 500);
+            }
+            e.preventDefault();
+        }
+    }
+    function hideSpollersBody(spollersBlock) {
+        const spollerActiveTitle = spollersBlock.querySelector("[data-spoller]._active");
+        if (spollerActiveTitle) {
+            spollerActiveTitle.classList.remove("_active");
+            _slideUp(spollerActiveTitle.nextElementSibling, 500);
+        }
+    }
+}
+
+
+
+// ===================================================================================================================
+// SlideToggle
+let _slideUp = (target, duration = 500) => {
+    if (!target.classList.contains("_slide")) {
+        target.classList.add("_slide");
+        target.style.transitionProperty = "height, margin, padding";
+        target.style.transitionDuration = duration + "ms";
+        target.style.height = target.offsetHeight + "px";
+        target.offsetHeight;
+        target.style.overflow = "hidden";
+        target.style.height = 0;
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBotoom = 0;
+        window.setTimeout(() => {
+            target.hidden = true;
+            target.style.removeProperty("height");
+            target.style.removeProperty("padding-top");
+            target.style.removeProperty("padding-bottom");
+            target.style.removeProperty("margin-top");
+            target.style.removeProperty("margin-bottom");
+            target.style.removeProperty("overflow");
+            target.style.removeProperty("transition-duration");
+            target.style.removeProperty("transition-property");
+            target.classList.remove("_slide");
+        }, duration);
+    }
+}
+let _slideDown = (target, duration = 500) => {
+    if (!target.classList.contains("_slide")) {
+        target.classList.add("_slide");
+        if (target.hidden) {
+            target.hidden = false;
+        }
+        let height = target.offsetHeight;
+        target.style.overflow = "hidden";
+        target.style.height = 0;
+        target.style.paddingTop = 0;
+        target.style.paddingBottom = 0;
+        target.style.marginTop = 0;
+        target.style.marginBotoom = 0;
+        target.offsetHeight;
+        target.style.transitionProperty = "height, margin, padding";
+        target.style.transitionDuration = duration + "ms";
+        target.style.height = height + "px";
+        target.style.removeProperty("padding-top");
+        target.style.removeProperty("padding-bottom");
+        target.style.removeProperty("margin-top");
+        target.style.removeProperty("margin-bottom");   
+        window.setTimeout(() => {
+            target.style.removeProperty("height");
+            target.style.removeProperty("overflow");
+            target.style.removeProperty("transition-duration");
+            target.style.removeProperty("transition-property");
+            target.classList.remove("_slide");
+        }, duration);
+    }
+}
+let  _slideToggle = (target, duration = 500) => {
+    if (target.hidden) {
+        return _slideDown(target, duration);
+    } else {
+        return _slideUp(target, duration);
+    }
+}
+
+/* 
+Для родителя спойлеров пишем атрибут data-spollers
+Для заголовка спойлеров пишем атрибут data-spoller
+Если нужно включать\выключать работу спойлеров на разных размерах экранов
+пишем параметры ширины и типа брейкпоинта.
+Например:
+data-spollers="992,max" - спойлеры будут работать только на экранах меньше или равно 992px
+data-spollers="768,min" - спойлеры будут работать только на экранах больше или равно 992px
+
+Если нужно чтобы в блоке открывался только один спойлер, добавляем атрибут data-one-spoller
+*/
+
+
+
+>>>>>>> Stashed changes
